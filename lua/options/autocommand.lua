6cccebdf3c6454a1bbccffc108ce5335b42971cc
daxmictar <dev@daxmictar.net>
2026-03-17 21:13:59 -0700

    smalled cleanup

--- lua/options/autocommand.lua
+++ lua/options/autocommand.lua
@@ -7,4 +7,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-
+-- auto-reload buffers when changed outside of nvim 
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+-- and then notify when a buffer is reloaded
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN)
+  end,
+})
--- lua/options/init.lua
+++ lua/options/init.lua
@@ -3,7 +3,7 @@ require "options.usercommand"
 require "options.keymap"
 require "options.lsp"
 
---[ FILE ]--
+-- [ FILE ]--
 vim.o.number = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
@@ -13,15 +13,17 @@ vim.o.relativenumber = true
 -- Replaces certain characters in the editor
 -- vim.o.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
---[ WINDOW ]--
+-- [ WINDOW ]--
 vim.o.splitbelow = true
 vim.o.splitright = true
 
---[ NATIVE ]--
+-- [ NATIVE ]--
 vim.o.mouse = 'a'
 vim.o.signcolumn = "yes"
 vim.o.swapfile = false
 vim.o.winborder = "single"
 vim.o.wrap = false
 
-
+-- [ BUFFER ] --
+vim.opt.updatetime = 1000 -- 1 second
+vim.opt.autoread = true
